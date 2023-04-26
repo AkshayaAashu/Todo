dcb1895c702a0bb8ea917b84ba5bf8e9eb9cdfbf
Akshaya <akshayaraj856@gmail.com>
2023-04-26 10:56:17 +0530

    msg

---
+++ todo/urls.py
@@ -0,0 +1,43 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+from crm import views as crm_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("todos/add/",views.TodoCreateView.as_view(),name="todo-add"),
+    path("todos/all/",views.TodoListView.as_view(),name="todo-list"),
+    path("todos/<int:pk>",views.TodoDetailView.as_view(),name="todo-detail"),
+    path("todos/<int:pk>/remove/",views.TodoDeleteView.as_view(),name="todo-delete"),
+    path("todos/<int:pk>/change/",views.TaskEditView.as_view(),name="todo-change"),
+    path("todos/completed/",views.TodoCompletedView.as_view(),name="todo-complete"),
+
+    path("employees/add/",crm_views.EmployeeCreateView.as_view(),name="emp-add"),
+    path("employees/all/",crm_views.EmployeeListView.as_view(),name="emp-list"),
+    path("employees/<int:pk>/detail/",crm_views.EmployeeDetailView.as_view(),name="emp-detail"),
+    path("employees/<int:pk>/remove/",crm_views.EmployeeDeleteView.as_view(),name="emp-remove"),
+    path("employees/<int:pk>/change/",crm_views.EmployeeEditView.as_view(),name="emp-change"),
+
+    path("register/",crm_views.SignUpView.as_view(),name="reg"),
+    path("login/",crm_views.SignInView.as_view(),name="signin"),
+    path("logout/",crm_views.signout_view,name="signout"),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
